7e5916b4c4180e0aecf92f1dc814590a334e9b54
jalejandro98 <juan-af98@hotmail.com>
2023-05-19 18:16:02 -0500

    Juan Alejandro: 18:15

--- app/src/main/java/uta/fisei/juan_alejandro_prueba_02/MainActivity.java
+++ app/src/main/java/uta/fisei/juan_alejandro_prueba_02/MainActivity.java
@@ -2,16 +2,18 @@ package uta.fisei.juan_alejandro_prueba_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button siguiente;
     private Button cerrar;
+    private EditText a;
+    private EditText b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-
+    
 
 }
+
