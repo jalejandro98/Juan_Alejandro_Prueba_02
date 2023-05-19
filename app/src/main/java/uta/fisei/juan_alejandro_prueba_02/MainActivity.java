c5c4118da971063b2463adba0a9a45208be59450
jalejandro98 <juan-af98@hotmail.com>
2023-05-19 17:46:02 -0500

    Juan Alejandro: 17:45

--- app/src/main/java/uta/fisei/juan_alejandro_prueba_02/MainActivity.java
+++ app/src/main/java/uta/fisei/juan_alejandro_prueba_02/MainActivity.java
@@ -3,12 +3,25 @@ package uta.fisei.juan_alejandro_prueba_02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button siguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        siguiente = findViewById(R.id.button_siguiente);
+
     }
+
+    siguiente.OnClickListener(){
+
+    }
+
+
 }
