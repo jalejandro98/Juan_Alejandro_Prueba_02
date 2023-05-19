daa1a309e208fc1d50d57d51f3add736cd68a541
jalejandro98 <juan-af98@hotmail.com>
2023-05-19 18:10:51 -0500

    Juan Alejandro: 18:10

--- app/src/main/java/uta/fisei/juan_alejandro_prueba_02/MainActivity.java
+++ app/src/main/java/uta/fisei/juan_alejandro_prueba_02/MainActivity.java
@@ -2,6 +2,7 @@ package uta.fisei.juan_alejandro_prueba_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,18 +11,26 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button siguiente;
+    private Button cerrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        siguiente = findViewById(R.id.button_siguiente);
+        siguiente = findViewById(R.id.button);
+        cerrar = findViewById(R.id.button2);
+        cerrar.setEnabled(false);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_segundo_alejandro);
+                cerrar = findViewById(R.id.button2);
+                cerrar.setEnabled(false);
+            }
+        });
 
     }
 
-    siguiente.OnClickListener(){
-
-    }
 
 
 }
--- app/src/main/java/uta/fisei/juan_alejandro_prueba_02/SegundoActivity_Alejandro.java
+++ app/src/main/java/uta/fisei/juan_alejandro_prueba_02/SegundoActivity_Alejandro.java
@@ -3,12 +3,17 @@ package uta.fisei.juan_alejandro_prueba_02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SegundoActivity_Alejandro extends AppCompatActivity {
+
+    private Button cerrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_segundo_alejandro);
+
+
     }
+
 }
